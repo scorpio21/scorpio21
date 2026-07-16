d31c0d2a272c53770d930520ddef5f77c19a53f5
Lopera <scorpio21@users.noreply.github.com>
2026-07-17 01:42:23 +0200

    Agregar soporte para objetos en el bloque de información del Pokémon en el README

--- scripts/update-readme.py
+++ scripts/update-readme.py
@@ -102,6 +102,7 @@ pokemon_info_block = build_pokemon_info_block(
     bebe=bebe,
     forma_regional=forma_regional,
     movimientos=movimientos,
+    objetos=objetos,
     juegos=juegos,
     cadena_evolucion=cadena_evolucion,
     bst_html=bst_html,
