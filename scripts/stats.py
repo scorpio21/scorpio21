f0b76dcde2b0b8c496304a1873ceade23434d85f
Lopera <scorpio21@users.noreply.github.com>
2026-07-17 10:22:42 +0200

    Actualizar la presentación de estadísticas de Pokémon a formato de texto en lugar de tabla

--- scripts/stats.py
+++ scripts/stats.py
@@ -58,44 +58,23 @@ def compute_bst(stats):
 """
 
     stats_md = f"""
-        <table>
+    ❤️ <b>PS</b> <b>{stats['hp']}</b><br>
+    {barra_stat(stats['hp'])}<br><br>
 
-        <tr>
-        <td>❤️ <b>PS</b></td>
-        <td>{barra_stat(stats['hp'])}</td>
-        <td><b>{stats['hp']}</b></td>
-        </tr>
+    ⚔️ <b>Ataque</b> <b>{stats['attack']}</b><br>
+    {barra_stat(stats['attack'])}<br><br>
 
-        <tr>
-        <td>⚔️ <b>Ataque</b></td>
-        <td>{barra_stat(stats['attack'])}</td>
-        <td><b>{stats['attack']}</b></td>
-        </tr>
+    🛡️ <b>Defensa</b> <b>{stats['defense']}</b><br>
+    {barra_stat(stats['defense'])}<br><br>
 
-        <tr>
-        <td>🛡️ <b>Defensa</b></td>
-        <td>{barra_stat(stats['defense'])}</td>
-        <td><b>{stats['defense']}</b></td>
-        </tr>
+    ✨ <b>At. Especial</b> <b>{stats['special-attack']}</b><br>
+    {barra_stat(stats['special-attack'])}<br><br>
 
-        <tr>
-        <td>✨ <b>At. Especial</b></td>
-        <td>{barra_stat(stats['special-attack'])}</td>
-        <td><b>{stats['special-attack']}</b></td>
-        </tr>
+    🛡️ <b>Def. Especial</b> <b>{stats['special-defense']}</b><br>
+    {barra_stat(stats['special-defense'])}<br><br>
 
-        <tr>
-        <td>🛡️ <b>Def. Especial</b></td>
-        <td>{barra_stat(stats['special-defense'])}</td>
-        <td><b>{stats['special-defense']}</b></td>
-        </tr>
-
-        <tr>
-        <td>💨 <b>Velocidad</b></td>
-        <td>{barra_stat(stats['speed'])}</td>
-        <td><b>{stats['speed']}</b></td>
-        </tr>
-
-        </table>
-        """
+    💨 <b>Velocidad</b> <b>{stats['speed']}</b><br>
+    {barra_stat(stats['speed'])}
+    """
+    
     return bst, nivel_bst, bst_html, stats_md
