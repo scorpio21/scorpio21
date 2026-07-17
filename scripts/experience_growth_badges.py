345fda076138e8a60470fbffdc1dbc8f7d1b42a9
Lopera <scorpio21@users.noreply.github.com>
2026-07-17 02:35:09 +0200

    Agregar soporte para badges de crecimiento de experiencia y optimizar la lista de objetos en la tabla de Pokémon

---
+++ scripts/experience_growth_badges.py
@@ -0,0 +1,8 @@
+EXPERIENCE_GROWTH_BADGES = {
+    "Errático": ("Errático", "purple"),
+    "Rápido": ("Rápido", "blue"),
+    "Medio rápido": ("Medio rápido", "brightgreen"),
+    "Medio lento": ("Medio lento", "yellow"),
+    "Lento": ("Lento", "red"),
+    "Fluctuante": ("Fluctuante", "orange"),
+}
