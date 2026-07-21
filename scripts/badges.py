68538b13fd454bf073db4983f512649763653c05
Lopera <scorpio21@users.noreply.github.com>
2026-07-21 14:56:44 +0200

    Ajustar el ancho de la imagen en la función build_tipos_html

--- scripts/badges.py
+++ scripts/badges.py
@@ -59,7 +59,7 @@ def build_tipos_html(tipos_es):
 
         if badge:
             html.append(
-                f'<img src="{badge}" alt="{tipo}" width="23" '
+                f'<img src="{badge}" alt="{tipo}" width="43" '
                 f'style="vertical-align:middle;">'
             )
 
