97a756e61c1e3d1d557127bbda23d60f68e75bc5
Lopera <scorpio21@users.noreply.github.com>
2026-07-21 15:11:22 +0200

    Agregar colores de tipos en la función build_tipos_html

--- scripts/badges.py
+++ scripts/badges.py
@@ -43,6 +43,27 @@ def build_badge_rareza(nombre, rareza):
 #------------------------------
 # Tipos Html
 #------------------------------
+TYPE_COLORS = {
+    "Normal": "#A8A878",
+    "Fuego": "#F08030",
+    "Agua": "#6890F0",
+    "Eléctrico": "#F8D030",
+    "Planta": "#78C850",
+    "Hielo": "#98D8D8",
+    "Lucha": "#C03028",
+    "Veneno": "#A040A0",
+    "Tierra": "#E0C068",
+    "Volador": "#A890F0",
+    "Psíquico": "#F85888",
+    "Bicho": "#A8B820",
+    "Roca": "#B8A038",
+    "Fantasma": "#705898",
+    "Dragón": "#7038F8",
+    "Siniestro": "#705848",
+    "Acero": "#B8B8D0",
+    "Hada": "#EE99AC",
+    "Astral": "#5A4FFF",
+}
 
 def build_tipos_html(tipos_es):
     html = []
@@ -69,6 +90,7 @@ def build_tipos_html(tipos_es):
         """)
 
     return "".join(html)
+
 
 #------------------------------
 # Debilidades, resistencias e inmunidades en HTML
