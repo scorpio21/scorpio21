51468de7751a4c6a1a53d1bad2e0b19d24a3aa4f
Lopera <scorpio21@users.noreply.github.com>
2026-07-21 17:43:25 +0200

    Eliminar salto de línea innecesario en la tabla de información de Pokémon

--- scripts/builders/pokemon_table_template.py
+++ scripts/builders/pokemon_table_template.py
@@ -290,8 +290,7 @@ def render_table(
 <tr>
 <td><b>📈 Nivel 100</b></td>
 <td>
-💠 <b>{f"{experience_to_level:,}".replace(",", ".")} XP</b><br>
-<small>Necesaria para alcanzar el nivel 100</small>
+💠 <b>{f"{experience_to_level:,}".replace(",", ".")} XP</b>
 </td>
 </tr>
 
