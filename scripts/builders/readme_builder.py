b493eea3504256f4e9def41972971e20c89105fe
scorpio21 <scorpio21@users.noreply.github.com>
2026-07-27 14:04:02 +0200

    Mejorar formato de curiosidad y juego de debut en el bloque de información de Pokémon

--- scripts/builders/readme_builder.py
+++ scripts/builders/readme_builder.py
@@ -120,13 +120,11 @@ def build_pokemon_info_block(
     )
 
     curiosidad_html = f"""
-    > 💬 **{curiosidad}**
+    💬 <b>{curiosidad}</b><br><br>
 
-    ---
+    🎮 <b>Juego de debut:</b> {juego_debut}<br>
 
-    🎮 **Juego de debut:** {juego_debut}
-
-    🌍 **Región:** {generation.split("(")[1].replace(")", "")}
+    🌍 <b>Región:</b> {generation.split("(")[1].replace(")", "")}
     """
 
     return f"""<!-- POKEMON_INFO -->
